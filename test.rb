628041c664f2cb411c8757029df1f90c0a1e69c9
guilherme <devguilhermeribeiro000@gmail.com>
2024-07-09 11:18:51 -0300

    ADICIONANDO O ARQUIVO DE TESTES, PARA TENTAR ALGUMAS COISAS ANTES DE IMPLEMENTAR

---
+++ test.rb
@@ -0,0 +1,29 @@
+class Post
+  attr_accessor :id, :title, :content, :created_at
+
+  def initialize(title, content)
+    @title = title
+    @content = content
+  end
+end
+
+class Database
+  def select_all_data
+    [
+      {'id' => 1, 'title' => 'First Post', 'content' => 'This is the first post', 'created_at' => '2023-01-01'},
+      {'id' => 2, 'title' => 'Second Post', 'content' => 'This is the second post', 'created_at' => '2023-01-02'}
+    ]
+  end
+end
+
+db = Database.new
+
+posts = db.select_all_data.map do |row|
+  Post.new(row['title'], row['content']).tap do |post|
+    post.id = row['id']
+    post.created_at = row['created_at']
+  end
+end
+
+# `posts` agora contém uma array de objetos `Post` completamente inicializados
+puts posts[0].id

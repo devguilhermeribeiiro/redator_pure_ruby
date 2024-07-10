4c8031c143cc7671bd3bbc74d8eeac686d88b0dc
guilherme <devguilhermeribeiro000@gmail.com>
2024-07-10 15:35:19 -0300

    Adicionando e configurando tailwind css no projeto

--- config/config.ru
+++ config/config.ru
@@ -1,3 +1,7 @@
 require_relative '../app/app.rb'
+require 'rack'
+
+
+use Rack::Static, :urls => ["/css", "/js", "/images"], :root => "public"
 
 run App.new
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.html.erb",
+    "./views/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

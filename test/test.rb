d28301d85f5cadbbc23b54dc5f0455ea1e00d1b7
guilherme <devguilhermeribeiro000@gmail.com>
2024-07-12 14:45:19 -0300

    Mais alguns testes

--- test/test.rb
+++ test/test.rb
@@ -1,21 +1,14 @@
 class Test
-  attr_accessor :new_table
-  def initialize(new_table)
-    @new_table = new_table
-    @table_name = @new_table[:table_name]
-    @table_params = @new_table[:table_params]
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
   end
-  def create_table
-    puts "CREATE TABLE IF NOT EXISTS #{@table_name} (
-    id SERIAL PRIMARY KEY,
-    #{@table_params.keys[0]} VARCHAR(255) NOT NULL,
-    #{@table_params.keys[1]} VARCHAR (255) NOT NULL UNIQUE,
-    #{@table_params.keys[2]} VARCHAR (255) NOT NULL,
-    )"
+
+  def hello
+    puts "Hello #{@name}, your age is #{@age}"
   end
 end
 
-a = {table_name: 'users', table_params: {full_name: 'John Snow', email: 'john@snow', password: 'johntargeryan'}}
-
-test = Test.new(a)
-test.create_table
+Test.new('Guilherme', '21').hello

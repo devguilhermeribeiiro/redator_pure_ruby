207e5a9c52de064a1f9942bcc274dbbf33e778ad
guilherme <devguilhermeribeiro000@gmail.com>
2024-07-17 01:03:03 -0300

    Noting changes

--- test/test.rb
+++ test/test.rb
@@ -1,16 +1,14 @@
-# class Test
-#   attr_accessor :name, :age
+class Test
+  attr_accessor :name, :age
 
-#   def initialize(name, age)
-#     @name = name
-#     @age = age
-#   end
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
 
-#   def hello
-#     puts "Hello #{@name}, your age is #{@age}"
-#   end
-# end
+  def hello
+    puts "Hello #{@name}, your age is #{@age}"
+  end
+end
 
-# Test.new('Guilherme', '21').hello
-
-require_relative '../app/database'
+Test.new('Guilherme', '21').hello

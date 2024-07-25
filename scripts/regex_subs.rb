1ba8eae803857b0acf2f88996a60ac5661b6ea95
guilherme <devguilhermeribeiro000@gmail.com>
2024-07-24 21:49:43 -0300

    Alterando o regex pra trocar as URIs pra mim

--- scripts/regex_subs.rb
+++ scripts/regex_subs.rb
@@ -1,8 +1,6 @@
 file_path = 'app/app.rb'
 content = File.read(file_path)
 
-replaced_word = content.gsub(/(redator|Redator)/) do |match|
-  match == 'Redator' ? 'Article' : 'article'
-end
+replaced_word = content.gsub('/admin/', '/admin/admin_dashboard/')
 
 File.open(file_path, 'w') { |file| file.write(replaced_word) }

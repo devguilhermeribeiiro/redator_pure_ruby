38e97b1401e5a8da346eb7dec997cbf6f2460d1d
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-08 14:25:33 -0300

    Realocando o arquivo de migrate do app/ para scripts/db_migrate/

---
+++ scripts/db_migrate/migrate.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative '../../app/database'
+
+class Migrate
+  def self.db
+    @db = Database.new
+  end
+
+  def self.create_tables
+    db.exec <<-SQL
+      CREATE TABLE IF NOT EXISTS articles (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    SQL
+
+    db.exec <<-SQL
+      CREATE TABLE IF NOT EXISTS admin (
+        id UUID PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      );
+    SQL
+  end
+end
+
+Migrate.create_tables

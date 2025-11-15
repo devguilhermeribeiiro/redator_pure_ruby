962ec7b0883bfa4c7d49a053fbf3249117f2eb08
guilherme <devguilhermeribeiro000@gmail.com>
2025-11-15 09:39:18 -0300

    Refactoring views

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,18 @@
+require 'eredor'
+require_relative '../mappers/post_mapper'
+
 class PostController < Eredor::BaseController
   def index
-    'Hello World'
+    result = @post_mapper.all
+    @posts = result.map do |row|
+      {
+        id: row['id'],
+        title: row['title'],
+        description: row['description'],
+        created_at: row['created_at'],
+        updated_at: row['updated_at']
+      }
+    end
+    render('index')
   end
 end
